6b7bd6fae66b9fdcddaecde05fec07cd6b4598cc
Grishma <grishmakesariya95@gmail.com>
2018-08-26 12:24:16 +1000

    changes to loan.java

--- src/loan.java
+++ src/loan.java
@@ -5,43 +5,42 @@ import java.util.Date;
 @SuppressWarnings("serial")
 public class loan implements Serializable {
 	
-	public static enum LoanState { CURRENT, OVER_DUE, DISCHARGED };	//Changes from LOAN_STATE to LoanState
-	private int id; //chage from ID to id
-	private book b;	//chage from B to b
-	private member m; //chage from M to m
-	private Date d;	//chage from D to d
-	private LoanState state;	//Changes from LOAN_STATE to LoanState
-
+	public static enum LoanState { CURRENT, OVER_DUE, DISCHARGED };	//Changes from LOAN_STATE to LoanState, enumnaming convention
+	private int id; //chage from ID to id,naming convention
+	private book b;	//chage from B to b,naming convention
+	private member m; //chage from M to m,naming convention
+	private Date d;	//chage from D to d,naming convention
+	private LoanState state;	//Changes from LOAN_STATE to LoanState,enum naming convention
 	
 	public loan(int loanId, book book, member member, Date dueDate) {
-		this.id = loanId; //chage from ID to id
-		this.b = book;	//chage from B to b
-		this.m = member;   //chage from M to m
-		this.d = dueDate;   //chage from D to d
-		this.state = LoanState.CURRENT;	//Changes from LOAN_STATE to LoanState
+		this.id = loanId; //chage from ID to id,naming convention
+		this.b = book;	//chage from B to b,naming convention
+		this.m = member;   //chage from M to m,naming convention
+		this.d = dueDate;   //chage from D to d,naming convention
+		this.state = LoanState.CURRENT;	//Changes from LOAN_STATE to LoanState, enum naming convention
 	}
 
 	
 	public void checkOverDue() {
-		if (state == LoanState.CURRENT &&	//Changes from LOAN_STATE to LoanState
+		if (state == LoanState.CURRENT &&	//Changes from LOAN_STATE to LoanState, enum naming convention
 			Calendar.getInstance().Date().after(D)) {
-			this.state = LoanState.OVER_DUE;	//Changes from LOAN_STATE to LoanState			
+			this.state = LoanState.OVER_DUE;	//Changes from LOAN_STATE to LoanState,enum naming convention			
 		}
 	}
 
 	
 	public boolean isOverDue() {
-		return state == LoanState.OVER_DUE;	//Changes from LOAN_STATE to LoanState
+		return state == LoanState.OVER_DUE;	//Changes from LOAN_STATE to LoanState,enum naming convention
 	}
 
 	
 	public Integer getId() {
-		return id;	//chage from ID to id
+		return id;	//chage from ID to id,naming convention
 	}
 
 
 	public Date getDueDate() {
-		return d; //chage from D to d
+		return d; //chage from D to d,naming convention
 	}
 	
 	
@@ -49,29 +48,29 @@ public class loan implements Serializable {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		StringBuilder sb = new StringBuilder();
-		sb.append("Loan:  ").append(id).append("\n") 	//chage from ID to id
-		  .append("  Borrower ").append(m.getId()).append(" : ")	//chage from M to m
-		  .append(m.getLastName()).append(", ").append(m.getFirstName()).append("\n")	//chage from M to m
-		  .append("  Book ").append(b.ID()).append(" : " ) //chage from B to b
-		  .append(b.Title()).append("\n")	//chage from B to b
-		  .append("  DueDate: ").append(sdf.format(d)).append("\n")	//chage from D to d
+		sb.append("Loan:  ").append(id).append("\n") 	//chage from ID to id naming,naming convention
+		  .append("  Borrower ").append(m.getId()).append(" : ")	//chage from M to m,naming convention
+		  .append(m.getLastName()).append(", ").append(m.getFirstName()).append("\n")	//chage from M to m,naming convention
+		  .append("  Book ").append(b.ID()).append(" : " ) //chage from B to b,naming convention
+		  .append(b.Title()).append("\n")	//chage from B to b,naming convention
+		  .append("  DueDate: ").append(sdf.format(d)).append("\n")	//chage from D to d,naming convention
 		  .append("  State: ").append(state);		
 		return sb.toString();
 	}
 
 
 	public member Member() {
-		return m;  //chage from M to m
+		return m;  //chage from M to m,naming convention
 	}
 
 
 	public book Book() {
-		return b;	//chage from B to b
+		return b;	//chage from B to b,naming convention
 	}
 
 
 	public void Loan() {
-		state = LoanState.DISCHARGED;	//Changes from LOAN_STATE to LoanState		
+		state = LoanState.DISCHARGED;	//Changes from LOAN_STATE to LoanState,enumnaming convention		
 	}
 
 }
